163f4fc0b0e2a93c265e3f63d368b67f252432c0
Michal Franczel <michal.franczel@gmail.com>
2019-11-17 19:42:00 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Popolvar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Popolvar.dir/main.c.o"
+  "Popolvar.exe"
+  "Popolvar.pdb"
+  "libPopolvar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Popolvar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
