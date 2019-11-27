1b496a5f4ae927ed490581d9fca98f9e8f77047f
Michal Franczel <michal.franczel@gmail.com>
2019-11-27 19:41:27 +0100

    Fixed everything, every test passed..

--- main.c
+++ main.c
@@ -11,16 +11,16 @@
 //#define generator 'G'
 
 struct node {
-    int x;
-    int y;
-    unsigned int dist;
-    int state;
+    short x;
+    short y;
+    unsigned short dist;
+    unsigned short state;
     struct node *prev;
 };
 
 
 int is_gen_on(struct node* node) {
-    int state = node -> state;
+    unsigned short state = node -> state;
     if(state & 1 == 1) {
         return 1;
     }
@@ -34,14 +34,19 @@ struct teleport {
 
 struct teleport *t0, *t1, *t2, *t3, *t4, *t5, *t6, *t7, *t8, *t9 = NULL;
 int princesses[5][2];
+//short hsh[128][500][500];
+short ***hsh;
 
 void heap_insert(struct node **heap, int *heap_size, struct node *node){
     heap[*heap_size] = node;
     int index = *heap_size;
     (*heap_size)++;
+    hsh[heap[index] -> state][heap[index] -> y][heap[index] -> x] = (*heap_size) - 1;
 
     while(1) {
         if(heap[(index-1)/2] -> dist > heap[index] -> dist) {
+            hsh[heap[(index-1)/2] -> state][heap[(index-1)/2] -> y][heap[(index-1)/2] -> x] = index;
+            hsh[heap[index] -> state][heap[index] -> y][heap[index] -> x] = (index-1)/2;
             struct node *temp = heap[index];
             heap[index] = heap[(index-1)/2];
             heap[(index-1)/2] = temp;
@@ -51,12 +56,17 @@ void heap_insert(struct node **heap, int *heap_size, struct node *node){
             break;
         }
     }
+
+    hsh[node -> state][node -> y][node -> x] = index;
 }
 
 struct node* heap_min(struct node **heap, int *heap_size) {
     struct node* min = heap[0];
     int index = --(*heap_size);
+
+    hsh[heap[0] -> state][heap[0] -> y][heap[0] -> x] = -1;
     heap[0] = heap[index];
+    hsh[heap[index] -> state][heap[index] -> y][heap[index] -> x] = 0;
     heap[index] = NULL;
 
     int left_child;
@@ -69,9 +79,6 @@ struct node* heap_min(struct node **heap, int *heap_size) {
         right_child = 2 * index + 2;
         smaller = index;
 
-        struct node* l = heap[left_child];
-        struct node* r = heap[right_child];
-
         if(right_child < *heap_size  && heap[smaller] -> dist > heap[right_child] -> dist) {
             smaller = right_child;
         }
@@ -81,17 +88,20 @@ struct node* heap_min(struct node **heap, int *heap_size) {
 
         if(smaller == index) {break;}
 
+        hsh[heap[index] -> state][heap[index] -> y][heap[index] -> x] = smaller;
+        hsh[heap[smaller] -> state][heap[smaller] -> y][heap[smaller] -> x] = index;
         struct node *temp = heap[index];
         heap[index] = heap[smaller];
         heap[smaller] = temp;
         index = smaller;
     }
 
+    //printf("%d %d\n", hash[(heap[index] -> x) * (heap[index] -> y) * (heap[index] -> state)], index);
     return min;
 }
 
 int get_state(int p5, int p4, int p3, int p2, int p1, int drak, int generator) {
-    int state = 0;
+    unsigned short state = 0;
     if(generator != 0) {state |= 1;}
     if(drak != 0) {state |= 2;}
     if(p1 != 0) {state |= 4;}
@@ -102,13 +112,14 @@ int get_state(int p5, int p4, int p3, int p2, int p1, int drak, int generator) {
     return state;
 }
 
-struct node* find(struct node**heap, int heap_size, int x, int y, int state) {
-    for(int i = 0; i < heap_size; i++) {
-        if(heap[i] -> x == x && heap[i] -> y == y && heap[i] -> state == state) {
-            return heap[i];
-        }
+struct node* find(struct node**heap, int heap_size, int x, int y, unsigned short state, int *in) {
+    if(hsh[state][y][x] == -1) {
+        return NULL;
+    } else {
+        (*in) = hsh[state][y][x];
+        return heap[hsh[state][y][x]];
     }
-    return NULL;
+
 }
 
 int get_cost(char **mapa, int x, int y) {
@@ -128,15 +139,22 @@ int get_cost(char **mapa, int x, int y) {
     return 0;
 }
 
-void decrease_priority(struct node **heap, int heap_size, int x, int y, int state, int new_priority) {
-    struct node* temp = find(heap, heap_size, x, y, state);
+struct node* decrease_priority(struct node **heap, int heap_size, int x, int y, unsigned short state, int new_priority, struct node* prev) {
     int i;
+    struct node* temp = find(heap, heap_size, x, y,state,&i);
+    if(temp == NULL ) {
+        return NULL;
+    }
 
-
+    if( temp -> dist < new_priority) {
+        return temp;
+    }
+    temp -> prev = prev;
     temp -> dist = new_priority;
     while(1) {
-        struct node *temp2 = heap[(i-1)/2];
         if(heap[(i-1)/2] -> dist > heap[i] -> dist) {
+            hsh[heap[(i-1)/2] -> state][heap[(i-1)/2] -> y][heap[(i-1)/2] -> x] = i;
+            hsh[heap[i] -> state][heap[i] -> y][heap[i] -> x] = (i-1)/2;
             struct node *temp = heap[i];
             heap[i] = heap[(i-1)/2];
             heap[(i-1)/2] = temp;
@@ -146,6 +164,7 @@ void decrease_priority(struct node **heap, int heap_size, int x, int y, int stat
             break;
         }
     }
+    return temp;
 
 }
 
@@ -187,47 +206,77 @@ int get_princess_mask(int x, int y) {
     return j;
 }
 
-int get_n_from_state(int state) {
+int get_n_from_state(unsigned short state) {
     int n = 0;
     n += (state & 64)/64 + (state & 32)/32 + (state & 16)/16 + (state & 8)/8 + (state & 4)/4;
     return n;
 }
 
-void next(char **mapa, int n, int m, int x, int y, int state, int xx, int yy,struct node **heap, int heap_size, struct node* prev, int *princ_n) {
+void next(char **mapa, int n, int m, int x, int y, unsigned short state, int xx, int yy,struct node **heap, int *heap_size, struct node* prev, int *princ_n) {
     struct node* temp;
 
     if(mapa[y+yy][x+xx] == 'C' || mapa[y+yy][x+xx] == 'H' || (mapa[y+yy][x+xx] == 'D'  && (state & 2) != 0) || (mapa[y+yy][x+xx] == 'P' &&  ((state & 2) == 0 || ((get_princess_mask(x+xx, y+yy) & state) !=  0))) || (mapa[y+yy][x+xx] >= '0' && mapa[y+yy][x+xx] <= '9' && (state & 1) == 1)) {
-            temp = find(heap, heap_size, x+xx, y+yy, state);
+        temp = decrease_priority(heap, *heap_size, x+xx, y+yy, state,prev -> dist + get_cost(mapa, x, y), prev);
 
-            if(temp != NULL && prev -> dist != UINT_MAX && temp -> dist > prev -> dist + get_cost(mapa, x, y)) {
-                temp ->prev = prev;
-                decrease_priority(heap, heap_size, x+xx, y+yy, state,prev -> dist + get_cost(mapa, x, y));
-            } else {return;}
+        if(temp == NULL){
+            temp = malloc(sizeof(struct node));
+            temp -> x = x+xx;
+            temp -> y = y+yy;
+            temp -> dist = prev -> dist + get_cost(mapa, x, y);
+            temp -> prev = prev;
+            temp -> state = state;
+            heap_insert(heap, heap_size, temp);
+        }
     } else if(mapa[y+yy][x+xx] == 'G' && (state & 1) == 0) {
-        temp = find(heap, heap_size, x+xx, y+yy, state | 1);
-        if(temp != NULL && prev -> dist != UINT_MAX && temp -> dist > prev -> dist + get_cost(mapa, x, y)) {
+        temp = decrease_priority(heap, *heap_size, x+xx, y+yy, state | 1, prev -> dist + get_cost(mapa, x, y), prev);
+
+        if(temp == NULL){
+            temp = malloc(sizeof(struct node));
+            temp -> x = x+xx;
+            temp -> y = y+yy;
+            temp -> dist = prev -> dist + get_cost(mapa, x, y);
             temp -> prev = prev;
-            decrease_priority(heap, heap_size, x+xx, y+yy, temp -> state, prev -> dist + get_cost(mapa, x, y));
-        } else {return;}
+            temp -> state = state | 1;
+            heap_insert(heap, heap_size, temp);
+        }
     } else if(mapa[y+yy][x+xx] == 'D' && (state & 2) == 0) {
-        temp = find(heap, heap_size, x+xx, y+yy, state | 2);
-        if(temp != NULL && prev -> dist != UINT_MAX && temp -> dist > prev -> dist + get_cost(mapa, x, y)) {
+        temp = decrease_priority(heap, *heap_size, x+xx, y+yy, state | 2, prev -> dist + get_cost(mapa, x, y), prev);
+
+        if(temp == NULL) {
+            temp = malloc(sizeof(struct node));
+            temp -> x = x+xx;
+            temp -> y = y+yy;
+            temp -> dist = prev -> dist + get_cost(mapa, x, y);
             temp -> prev = prev;
-            decrease_priority(heap, heap_size, x+xx, y+yy, temp -> state, prev -> dist + get_cost(mapa, x, y));
-        } else {return;}
+            temp -> state = state | 2;
+            heap_insert(heap, heap_size, temp);
+        }
     } else if(mapa[y+yy][x+xx] == 'P' &&  (state & 2) != 0 && ((get_princess_mask(x+xx, y+yy) & state) ==  0)) {
-        temp = find(heap, heap_size, x+xx, y+yy, state | get_princess_mask(x+xx, y+yy));
-        if(temp != NULL && prev -> dist != UINT_MAX && temp -> dist > prev -> dist + get_cost(mapa, x, y)) {
+        temp = decrease_priority(heap, *heap_size, x+xx, y+yy, state | get_princess_mask(x+xx, y+yy), prev -> dist + get_cost(mapa, x, y), prev);
+
+        if(temp == NULL) {
+            temp = malloc(sizeof(struct node));
+            temp -> x = x+xx;
+            temp -> y = y+yy;
+            temp -> dist = prev -> dist + get_cost(mapa, x, y);
             temp -> prev = prev;
-            decrease_priority(heap, heap_size, x+xx, y+yy, temp -> state , prev -> dist + get_cost(mapa, x, y));
-        } else {return;}
+            temp -> state = state | get_princess_mask(x+xx, y+yy);
+            heap_insert(heap, heap_size, temp);
+        }
     } else if(mapa[y+yy][x+xx] >= '0' && mapa[y+yy][x+xx] <= '9' && (state & 1) == 1) {
         struct teleport* t = find_teleport(mapa[y+yy][x+xx]);
         while(t != NULL) {
-            temp = find(heap, heap_size, t ->x, t -> y, state);
-            if(temp != NULL && prev -> dist != UINT_MAX && temp -> dist > prev -> dist + get_cost(mapa, x, y)) {
+            int xxx = t -> x;
+            int yyy = t -> y;
+            temp = decrease_priority(heap, *heap_size, xxx, yyy, state, prev -> dist + get_cost(mapa, x, y), prev);
+            if(temp == NULL){
+                temp = malloc(sizeof(struct node));
+                temp -> x = xxx;
+                temp -> y = yyy;
+                temp -> dist = prev -> dist + get_cost(mapa, x, y);
                 temp -> prev = prev;
-                decrease_priority(heap, heap_size, x+xx, y+yy, state , prev -> dist + get_cost(mapa, x, y));
+                temp -> state = state;
+                heap_insert(heap, heap_size, temp);
             }
             t = t->next;
         }
@@ -237,34 +286,23 @@ void next(char **mapa, int n, int m, int x, int y, int state, int xx, int yy,str
 }
 
 int* shortest_dijkstra(char **mapa, int n, int m, int princ_n, int *dlzka_cesty) {
-    struct node **heap = (struct node**) malloc(n*m*(128)*sizeof(struct node*));
-    memset(heap, 0, n*m*(128)*sizeof(struct node*));
+    struct node **heap = (struct node**) malloc((n*m+1)*sizeof(struct node*));
+    memset(heap, 0, (n*m+1)*sizeof(struct node*));
     int heap_size = 0;
     int x, y;
     unsigned int dist;
-    int state;
+    unsigned short state;
     struct node* temp;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            for(int k = 0; k < 128; k++) {
-                temp = malloc(sizeof(struct node));
-                temp -> x = j;
-                temp -> y = i;
-                temp -> prev = NULL;
-                temp -> state = k;
-                if(i == 0 && j == 0 && k == 0) {
-                    temp -> dist = 0;
-                } else {
-                    temp -> dist = UINT_MAX;
-                }
-                heap_insert(heap, &heap_size, temp);
-            }
-        }
-    }
-
-    struct node **all_paths = malloc(500*sizeof(struct node*));
-    int no = 0;
+    temp = malloc(sizeof(struct node));
+    temp -> x = 0;
+    temp -> y = 0;
+    temp -> prev = NULL;
+    temp -> state = 0;
+    temp -> dist = 0;
+    heap_insert(heap, &heap_size, temp);
+    char arr[128][n][m];
+    memset(arr, 0, sizeof(arr));
 
 
     while(heap_size != 0) {
@@ -275,25 +313,25 @@ int* shortest_dijkstra(char **mapa, int n, int m, int princ_n, int *dlzka_cesty)
         dist = min -> dist;
 
 
-        if(x < m-1) {
-            next(mapa, n, m, x, y, min -> state, 1, 0, heap, heap_size, min, &princ_n);
+        if(x < m-1 && arr[state][y][x] != 1) {
+            next(mapa, n, m, x, y, min -> state, 1, 0, heap, &heap_size, min, &princ_n);
         }
 
-        if(x > 0) {
-            next(mapa, n, m, x, y, min -> state,-1, 0, heap, heap_size, min, &princ_n);
+        if(x > 0  && arr[state][y][x] != 1) {
+            next(mapa, n, m, x, y, min -> state,-1, 0, heap, &heap_size, min, &princ_n);
         }
 
-        if(y < n-1) {
-            next(mapa, n, m, x, y, min -> state, 0, 1, heap, heap_size, min, &princ_n);
+        if(y < n-1  && arr[state][y][x] != 1) {
+            next(mapa, n, m, x, y, min -> state, 0, 1, heap, &heap_size, min, &princ_n);
         }
 
-        if(y > 0 ) {
-            next(mapa, n, m, x, y, min -> state,0, -1, heap, heap_size, min, &princ_n);
+        if(y > 0  && arr[state][y][x] != 1) {
+            next(mapa, n, m, x, y, min -> state,0, -1, heap, &heap_size, min, &princ_n);
         }
         if(princ_n == get_n_from_state(state) && mapa[y][x] == 'P') {
 
             (*dlzka_cesty) = 0;
-            int *cesta = malloc(sizeof(int)*5000);
+            int *cesta = malloc(sizeof(int)*2500);
             int i = 0;
             while(min != NULL) {
                 //printf("%d %d %u %d\n", min -> x, min -> y, min -> dist, min -> state);
@@ -304,32 +342,15 @@ int* shortest_dijkstra(char **mapa, int n, int m, int princ_n, int *dlzka_cesty)
                 (*dlzka_cesty)++;
             }
 
-            int temp;
-            int j = 0;
-            while(i != j) {
-                temp = cesta[i];
-                cesta[i] = cesta[j];
-                cesta[j] = temp;
-                i--;
-                j++;
-            }
             return cesta;
         }
 
         heap_min(heap, &heap_size);
+        arr[state][y][x] = 1;
 
     }
 
-//    for(int i = 0; i < no; i++) {
-//        if(all_paths[i] -> prev != NULL) {
-//            struct node* min = all_paths[i];
-//            while(min != NULL) {
-//                printf("%d %d %d %d\n", min -> x, min -> y, min -> dist, min -> state);
-//                min = min -> prev;
-//            }
-//            printf("------------------\n");
-//        }
-//    }
+
 }
 
 void add_teleport(struct teleport *t, int x, int y) {
@@ -346,13 +367,19 @@ void add_teleport(struct teleport *t, int x, int y) {
 
 int *zachran_princezne(char **mapa, int n, int m, int t, int *dlzka_cesty)
 {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            printf("%c ", mapa[i][j]);
+    hsh = (short***)malloc(sizeof(short**)*128);
+    for(int j = 0; j < 128; j++) {
+        hsh[j] =  (short**) malloc(n*sizeof(short*));
+        for(int k = 0; k < n; k++) {
+            hsh[j][k] =  (short*)malloc(m*sizeof(short));
+            memset(hsh[j][k], -1, m*sizeof(short));
         }
-        printf("\n");
     }
+
+
+
     memset(princesses, 0, sizeof(princesses));
+    //memset(hsh, -1, sizeof(hsh));
     int drak[2];
     int princess_counter = 0;
     for(int i = 0; i < n; i++) {
@@ -411,15 +438,17 @@ int *zachran_princezne(char **mapa, int n, int m, int t, int *dlzka_cesty)
 
 int main() {
     int n = 5;
-    int m = 30;
+    int m = 30; //30
     int dlzka = 0;
-    /*char mapa[5][5] = {
+    /*
+    char mapa[5][5] = {
             {'P','P','H','H','H'},
             {'H','C','H','H','C'},
             {'C','C','H','C','C'},
             {'C','H','C','H','C'},
             {'H','P','C','D','C'}
     };*/
+
 
     char mapa[5][30] = {
             {'H', 'H', 'H', 'H', 'H', 'C', 'H', 'C', 'C', 'C', 'H', 'H', 'H', 'C', 'H', 'H', 'H', 'H', 'P', 'D', 'H', 'C', 'C', 'C', 'C', 'H', 'C', 'C', 'C', 'H'},
@@ -437,6 +466,7 @@ int main() {
     int i, *cesta = zachran_princezne(mapa_3, n, m, 10, &dlzka);
     for (i=0;i<dlzka;++i)
         printf("%d %d\n", cesta[i*2], cesta[i*2+1]);
-    //printf("%d\n", 1|2);
+    printf("%d\n", sizeof(hsh)/8/1024/1024);
+
     return 0;
 }
